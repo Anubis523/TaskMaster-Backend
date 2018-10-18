10fc6aa90cd776a977e790c7c4057c0394ff8612
skarimo <karimovj@gmail.com>
2018-10-18 14:44:16 -0400

    fixed associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :project
-  has_one :worker, through: :project
+  has_one :employer, through: :project
+  belongs_to :worker, optional: true
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,5 +1,5 @@
 class Worker < ApplicationRecord
   belongs_to :employer
-  has_one :project, through: :employer
+  has_many :projects, through: :employer
   has_many :tasks, through: :project
 end
